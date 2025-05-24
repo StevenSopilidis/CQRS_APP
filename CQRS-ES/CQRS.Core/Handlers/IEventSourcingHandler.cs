b3af516d8368265d954237cb8b8284e38325d776
StevenSopilidis <sopilidimaria@gmail.com>
2025-05-24 13:07:05 +0300

    pushed forgoten code

---
+++ CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
@@ -0,0 +1,11 @@
+using CQRS.Core.Domain;
+
+namespace CQRS.Core.Handlers 
+{
+    // where T ---> concrete aggregate implementation
+    public interface IEventSourcingHandler<T> 
+    {
+        Task SaveAsync(AggregateRoot aggregate);
+        Task<T> GetByIdAsync(Guid aggregateId);
+    }
+}
