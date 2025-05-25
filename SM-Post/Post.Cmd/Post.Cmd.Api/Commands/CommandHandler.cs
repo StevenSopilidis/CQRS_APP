970f3032f3ea8ec4cfa8016328e698c5eac3bcad
StevenSopilidis <sopilidimaria@gmail.com>
2025-05-25 09:21:22 +0300

    added command handling

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/CommandHandler.cs
@@ -0,0 +1,66 @@
+
+using CQRS.Core.Handlers;
+using Post.Cmd.Domain.Aggregates;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class CommandHandler : ICommandHandler
+    {
+        private readonly IEventSourcingHandler<PostAggregate> _handler;
+
+        public CommandHandler(IEventSourcingHandler<PostAggregate> handler)
+        {
+            _handler = handler;
+        }
+
+        public async Task HandleAsync(NewPostCommand command)
+        {
+            var aggregate = new PostAggregate(command.Id, command.Author, command.Message);
+            await _handler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(EditMessageCommand command)
+        {
+            var aggregate = await _handler.GetByIdAsync(command.Id);
+            aggregate.EditMessage(command.Message);
+            await _handler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(LikePostCommand command)
+        {
+            var aggregate = await _handler.GetByIdAsync(command.Id);
+            aggregate.LikePost();
+            await _handler.SaveAsync(aggregate);
+
+        }
+
+        public async Task HandleAsync(AddCommentCommand command)
+        {
+            var aggregate = await _handler.GetByIdAsync(command.Id);
+            aggregate.AddComment(command.Comment, command.Username);
+            await _handler.SaveAsync(aggregate);
+
+        }
+
+        public async Task HandleAsync(EditCommentCommand command)
+        {
+            var aggregate = await _handler.GetByIdAsync(command.Id);
+            aggregate.EditComment(command.CommentId, command.Comment, command.Username);
+            await _handler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(RemoveCommentCommand command)
+        {
+            var aggregate = await _handler.GetByIdAsync(command.Id);
+            aggregate.RemoveComment(command.CommentId, command.Username);
+            await _handler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(DeletePostCommand command)
+        {
+            var aggregate = await _handler.GetByIdAsync(command.Id);
+            aggregate.DeletePost(command.Username);
+            await _handler.SaveAsync(aggregate);
+        }
+    }
+}
--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,10 +1,14 @@
 using CQRS.Core.Domain;
 using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
 using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
+using ZstdSharp.Unsafe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +17,21 @@ builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameo
 
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// register command handlers
+var handler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(handler.HandleAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(handler.HandleAsync);
+dispatcher.RegisterHandler<LikePostCommand>(handler.HandleAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(handler.HandleAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(handler.HandleAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(handler.HandleAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(handler.HandleAsync);
+
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
