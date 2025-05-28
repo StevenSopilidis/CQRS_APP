4aeec7cf8e9b9a9c60ed3219e078172918833f46
StevenSopilidis <sopilidimaria@gmail.com>
2025-05-28 07:23:36 +0300

    implemented command handlers

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,10 @@ Action<DbContextOptionsBuilder> configureDbContext = (o =>
 
 builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
 builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
 
 // Create database and tables
 var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
